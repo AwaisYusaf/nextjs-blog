5aebc43da6431efe0a57cd0c6350b590452953ed
AwaisYusaf <muhammadawais6640@gmail.com>
2023-02-06 11:41:36 +0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+
+export function getStaticProps() {
+  return {
+    props: {
+      data: "hello this is sample data of getStaticProps",
+    },
+  };
+}
+
+export default function Home({ data }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+        <p>{data}</p>
+      </section>
+    </Layout>
+  );
+}
